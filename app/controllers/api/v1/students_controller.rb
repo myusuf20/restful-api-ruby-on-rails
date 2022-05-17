722d2960bccadf899058dd2ea2048053744c9131
Mohammad Yusuf <ysbcc.rys@gmail.com>
2022-05-17 12:18:26 +0700

    create api directory 'api/v1/' and create students controller as well as method index, show, and create

---
+++ app/controllers/api/v1/students_controller.rb
@@ -0,0 +1,30 @@
+module Api
+  module V1
+    class StudentsController < ApplicationController
+      def index
+        @students = Student.all
+        render json: @students
+      end
+
+      def show
+        @student = Student.find(params[:id])
+        render json: @student
+      end
+
+      def create
+        @student = Student.new(student_params)
+
+        if @student.save
+          render json: @student
+        else
+          render json: @student.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+      def student_params
+        params.require(:student).permit(:name, :address)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # api/v1/students
+  namespace :api do
+    namespace :v1 do
+      resources :students
+    end
+  end
 end
